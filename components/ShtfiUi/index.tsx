00ef934e85241d4de1c21d89cfeabefb62aa06a7
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-11-08 12:36:29 +0000

    [5] -- Add export for footer tile component

--- components/ShtfiUi/index.tsx
+++ components/ShtfiUi/index.tsx
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as NavBar } from "./NavBar";
 export { default as Heading } from "./Heading";
 export { default as Footer } from "./Footer";
+export { default as FooterTile } from "./FooterTile";
