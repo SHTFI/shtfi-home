d53e4c04ca08309b030955e40b68fddc7f795ce6
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-11-08 11:48:05 +0000

    -- export footer component

--- components/ShtfiUi/index.tsx
+++ components/ShtfiUi/index.tsx
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as NavBar } from "./NavBar";
 export { default as Heading } from "./Heading";
+export { default as Footer } from "./Footer";
