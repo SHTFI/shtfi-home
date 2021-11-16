68ba6af53d09c63d04e0ae6404d624a3574d5486
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-11-16 07:19:35 +0000

    [3] -- Import nav bar [8 - EOF] -- Add temp implementation of the nav bar while I dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,75 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { NavBar } from "components";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavBar
+        linkList={[
+          {
+            href: "/",
+            title: "A link",
+            label: "A Label",
+            iconAlt: "An icon",
+            icon: "/assets/icons/shtfi.svg",
+          },
+          {
+            href: "/",
+            title: "A link",
+            label: "A Label",
+            iconAlt: "An icon",
+            icon: "/assets/icons/shtfi.svg",
+          },
+          {
+            href: "/",
+            title: "A link",
+            label: "A Label",
+            iconAlt: "An icon",
+            icon: "/assets/icons/shtfi.svg",
+          },
+          {
+            href: "/",
+            title: "A link",
+            label: "A Label",
+            iconAlt: "An icon",
+            icon: "/assets/icons/shtfi.svg",
+          },
+          {
+            href: "/",
+            title: "A link",
+            label: "A Label",
+            iconAlt: "An icon",
+            icon: "/assets/icons/shtfi.svg",
+          },
+          {
+            href: "/",
+            title: "A link",
+            label: "A Label",
+            iconAlt: "An icon",
+            icon: "/assets/icons/shtfi.svg",
+          },
+          {
+            href: "/",
+            title: "A link",
+            label: "A Label",
+            iconAlt: "An icon",
+            icon: "/assets/icons/shtfi.svg",
+          },
+          {
+            href: "/",
+            title: "A link",
+            label: "A Label",
+            iconAlt: "An icon",
+            icon: "/assets/icons/shtfi.svg",
+          },
+        ]}
+        logo="/assets/icons/shtfi.svg"
+        brandWording="SHTFI"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
