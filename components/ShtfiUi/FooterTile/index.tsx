7150835654f69d00b44a8063636758d6deb16acd
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-01 06:56:32 +0000

    Style component [1] import SC [8 - 26] -- Add styling with SC

--- components/ShtfiUi/FooterTile/index.tsx
+++ components/ShtfiUi/FooterTile/index.tsx
@@ -1,7 +1,27 @@
+import styled from "styled-components";
 import { FooterTileProps } from "../types";
 
 const FooterTile: React.FC<FooterTileProps> = ({ children, ...rest }) => {
-  return <div {...rest}>{children}</div>;
+  return <StyledFooterTile {...rest}>{children}</StyledFooterTile>;
 };
 
+const StyledFooterTile = styled.div`
+  display: flex;
+  flex-direction: column;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    width: auto;
+    align-self: center;
+    min-width: 20ch;
+
+    li {
+      margin: 0;
+      padding: 0;
+      font-weight: 100;
+      font-size: var(--meta-font);
+    }
+  }
+`;
 export default FooterTile;
