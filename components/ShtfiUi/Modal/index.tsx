a806ead295f51b6f1e840611851efbaeab6c4c1b
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-01 06:59:08 +0000

    Style modal [1] add SC [30 - 35] -- Ensure the callback can be triggered by the modal open button [46] -- Add the data-type attr to the modal open button [49 - 62] -- USe styled components instead of native ones [67 - EOF] -- Add styling components

--- components/ShtfiUi/Modal/index.tsx
+++ components/ShtfiUi/Modal/index.tsx
@@ -1,6 +1,7 @@
 import { ModalProps } from "../types";
 import { useState } from "react";
 import { Button } from "components";
+import styled from "styled-components";
 
 const Modal: React.FC<ModalProps> = ({
   open = false,
@@ -26,7 +27,12 @@ const Modal: React.FC<ModalProps> = ({
     // The event didn't stem from our desired components
     if (!!!dataAttr) return;
     // Ensure it was either the close button or underlay which triggered the event
-    if (dataAttr !== "underlay" && dataAttr !== "modal-close") return;
+    if (
+      dataAttr !== "underlay" &&
+      dataAttr !== "modal-close" &&
+      dataAttr !== "modal-open"
+    )
+      return;
     // Toggle the modal
     setIsOpen(!isOpen);
     // Call the callback
@@ -37,21 +43,86 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <>
-      <Button {...buttonProps}>{!!buttonLabel ? buttonLabel : "Open"}</Button>
-      <div
+      <Button data-type="modal-open" {...buttonProps} onClick={handleToggle}>
+        {!!buttonLabel ? buttonLabel : "Open"}
+      </Button>
+      <StyledModalWrapper
         data-type="wrapper"
         aria-hidden={!isOpen}
         onClick={handleToggle}
         {...wrapperProps}
       >
-        <span data-type="underlay" {...underlayProps}></span>
-        <div {...rest}>
-          <button data-type="modal-close"></button>
+        <StyledUnderlay data-type="underlay" {...underlayProps} />
+        <StyledModal aria-hidden={!isOpen} {...rest}>
           {children}
-        </div>
-      </div>
+          <Button size="med" data-type="modal-close" onClick={handleToggle}>
+            Close
+          </Button>
+        </StyledModal>
+      </StyledModalWrapper>
     </>
   );
 };
 
+const StyledModalWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  &[aria-hidden="true"] {
+    z-index: -1;
+    opacity: 0;
+    transition: opacity 0.3s ease, max-height 0s 0.3s, visibility 0s 0.3s,
+      z-index 0s 0.3s;
+  }
+
+  &[aria-hidden="false"] {
+    z-index: 999;
+    opacity: 1;
+    transition: opacity 0.3s ease 0.1s, max-height 0s 0s, visibility 0s 0s,
+      z-index 0s 0s;
+  }
+`;
+const StyledUnderlay = styled.span`
+  background-color: rgba(0, 0, 0, 0.7);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  content: "";
+  top: 0;
+  left: 0;
+`;
+const StyledModal = styled.aside`
+  top: 50%;
+  left: 50%;
+  background-color: var(--light);
+  z-index: 999;
+  position: absolute;
+  padding: var(--large-space);
+  border-radius: var(--radius-large);
+  width: 95%;
+  height: auto;
+  max-height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &[aria-hidden="true"] {
+    transform: translate(-50%, 100%);
+    transition: transform 0.3s ease;
+  }
+
+  &[aria-hidden="false"] {
+    transform: translate(-50%, -50%);
+    transition: transform 0.3s ease 0.1s;
+  }
+
+  > button:last-child {
+    margin-top: var(--large-space);
+  }
+`;
 export default Modal;
