416f15d363e4fc2d644c133a0a253ea11e616f52
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-01 06:55:50 +0000

    Style footer [1] -- import styled components [8 - 12] -- Add styling

--- components/ShtfiUi/Footer/index.tsx
+++ components/ShtfiUi/Footer/index.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components";
 import { FooterProps } from "../types";
 
 const Footer: React.FC<FooterProps> = ({ children, ...rest }) => {
-  return <footer {...rest}>{children}</footer>;
+  return <StyledFooter {...rest}>{children}</StyledFooter>;
 };
 
+const StyledFooter = styled.footer`
+  background-color: var(--blue);
+  padding: var(--large-space);
+  color: var(--light);
+`;
 export default Footer;
