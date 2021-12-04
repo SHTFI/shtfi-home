cfcd645125de9b3710f04a1ac4e16a203e8b7025
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-04 13:13:39 +0000

    Add export for SocialLinks

--- components/Base/index.tsx
+++ components/Base/index.tsx
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Card } from "./Card";
 export { default as Tokenomics } from "./Tokenomics";
+export { default as SocialLinks } from "./SocialLinks";
