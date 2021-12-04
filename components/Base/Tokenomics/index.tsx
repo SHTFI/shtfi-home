8cf9442f1198efa0701e53bcefed850bf4e13d16
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-04 13:31:23 +0000

    Removed the separating row so that all data is presented in a single flex row
    [75] -- Ensure the content is always positioned within the visible part of the section
    [79] -- Make the element relatively position so we can utilise pseudo elements for the transparent bg image
    [81 - 92] -- Add a transparent bg image
    [100] -- Make the main content wrapper a flex row
    [103] -- Ensure it wraps
    [104] -- Conter the content
    [107,108] -- Ensure the header retains its place as a single line
    [119] -- increase space
    [128] -- Ensure text stays on one line
    [136] -- Ensure text stays on one line
    [141] -- lower font size
    [147] -- lower font size

--- components/Base/Tokenomics/index.tsx
+++ components/Base/Tokenomics/index.tsx
@@ -20,6 +20,30 @@ const Tokenomics: React.FC<TokenomicsProps> = ({
       <StyledTokenomicsContent>
         <Heading level={2} text="Tokenomics" />
         <StyledTokenomicsData
+          data-large={showLarge?.includes("devFund") ? "true" : "false"}
+        >
+          <span>{`${devFund}${
+            showPercent?.includes("devFund") ? "%" : ""
+          }`}</span>
+          <h4>Dev Fund</h4>
+        </StyledTokenomicsData>
+        <StyledTokenomicsData
+          data-large={showLarge?.includes("preMine") ? "true" : "false"}
+        >
+          <span>{`${preMine}${
+            showPercent?.includes("preMine") ? "%" : ""
+          }`}</span>
+          <h4>Pre Mine</h4>
+        </StyledTokenomicsData>
+        <StyledTokenomicsData
+          data-large={showLarge?.includes("marketing") ? "true" : "false"}
+        >
+          <span>{`${marketing}${
+            showPercent?.includes("marketing") ? "%" : ""
+          }`}</span>
+          <h4>Marketing</h4>
+        </StyledTokenomicsData>
+        <StyledTokenomicsData
           data-large={showLarge?.includes("supply") ? "true" : "false"}
         >
           <span>{`${supply}${
@@ -27,32 +51,6 @@ const Tokenomics: React.FC<TokenomicsProps> = ({
           }`}</span>
           <h4>Max Supply</h4>
         </StyledTokenomicsData>
-        <StyledTokenomicsRow>
-          <StyledTokenomicsData
-            data-large={showLarge?.includes("devFund") ? "true" : "false"}
-          >
-            <span>{`${devFund}${
-              showPercent?.includes("devFund") ? "%" : ""
-            }`}</span>
-            <h4>Dev Fund</h4>
-          </StyledTokenomicsData>
-          <StyledTokenomicsData
-            data-large={showLarge?.includes("preMine") ? "true" : "false"}
-          >
-            <span>{`${preMine}${
-              showPercent?.includes("preMine") ? "%" : ""
-            }`}</span>
-            <h4>Pre Mine</h4>
-          </StyledTokenomicsData>
-          <StyledTokenomicsData
-            data-large={showLarge?.includes("marketing") ? "true" : "false"}
-          >
-            <span>{`${marketing}${
-              showPercent?.includes("marketing") ? "%" : ""
-            }`}</span>
-            <h4>Marketing</h4>
-          </StyledTokenomicsData>
-        </StyledTokenomicsRow>
         <StyledTokenomicsData
           data-large={showLarge?.includes("farm") ? "true" : "false"}
         >
@@ -74,64 +72,79 @@ const StyledTokenomics = styled.section<{ color?: string }>`
       };
       color: white;
       position: relative;
-      padding: calc(var(--large-space) * 6) var(--large-space);
+      padding: calc(var(--large-space) * 6) 25%;
       transform: rotate(5deg);
-      width: 120%;
-      margin: calc(var(--large-space) * 4) 0 calc(var(--large-space) * 4) -10%;
+      width: 150%;
+      margin: calc(var(--large-space) * 4) 0 calc(var(--large-space) * 4) -25%;
+      position: relative;
+      
+      &::before {
+        content: "";
+        position: absolute;
+        height: 50%;
+        width: 75%;
+        background: url("assets/icons/plunger.svg");
+        background-repeat: no-repeat;
+        background-position-x: right;
+        background-position-y: center;
+        z-index: -1;
+        opacity: 0.1;
+        left: 0;
+      }
       `;
   }}
 `;
 
 const StyledTokenomicsContent = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   transform: rotate(-5deg);
-  margin: 0 5%;
+  margin: 0;
+  flex-wrap: wrap;
+  justify-content: center;
 
   > h2 {
+    width: 100%;
+    flex-basis: 100%;
     margin-bottom: calc(var(--large-space) * 4);
   }
 `;
 
-const StyledTokenomicsRow = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: calc(var(--large-space) * 3) 0;
-`;
 const StyledTokenomicsData = styled.div<{ color?: string }>`
   ${({ color }): string => {
     return `
       display: flex;
       flex-direction: column;
       width: auto;
-      margin: var(--med-space);
+      margin: var(--med-space) var(--large-space);
       justify-content: center;
-      white-space: no-wrap;
       align-items: center;
-
+      
       > h4 {
         font-weight: 100;
         margin: 0;
         font-size: var(--meta-font);
         margin: calc(var(--large-space)) 0;
+        white-space: no-wrap;
       }
-
+      
       > span {
-        font-size: calc(var(--head-font) * 1.5);
+        font-size: var(--head-font);
         font-weight: 900;
         letter-spacing: 1.5px;
         line-height: 1;
+        white-space: no-wrap;
       }
 
       &[data-large="true"] {
         > span {
-          font-size: calc(var(--head-font) * 2.5);
+          font-size: calc(var(--head-font) * 1.5);
           color: var(--light-blue);
           text-shadow: var(--shadow);
         }
 
         > h4 {
-          font-size: var(--head-font);
+          font-size: var(--reg-font);
         }
       }
       `;
