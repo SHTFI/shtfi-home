b3b64f50568964e901ce82db02fa1b2111188290
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-04 16:59:58 +0000

    Add site fata file

---
+++ utils/config/siteData.tsx
@@ -0,0 +1,12 @@
+import { SocialLinksData } from "types";
+
+export const socialLinksData: SocialLinksData = {
+  github: {
+    profileName: "SHTFI",
+    profileUrl: "https://github.com/SHTFI",
+  },
+  twitter: {
+    profileName: "SHTFI",
+    profileUrl: "https://twitter.com/shtfio",
+  },
+};
