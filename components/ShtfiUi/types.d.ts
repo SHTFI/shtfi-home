c379e08f83827b8dfd6352d94ba353495417c2ae
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-05 20:16:11 +0000

    Add types for input and moved base components

--- components/ShtfiUi/types.d.ts
+++ components/ShtfiUi/types.d.ts
@@ -103,3 +103,39 @@ export interface TextIconProps extends React.HTMLAttributes<HTMLDivElement> {
   copy: string;
   icon: string;
 }
+
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
+  button?: boolean;
+}
+
+import { Colors, CoinDistribution } from "types";
+
+export interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string;
+  subTitle?: string;
+  logo?: string;
+  logoAlt?: string;
+  socialIcons?: boolean;
+}
+
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  padding?: string;
+  margin?: string;
+  maxWidth?: string;
+  maxHeight?: string;
+  minHeight?: string;
+  minWidth?: string;
+  background?: string;
+}
+
+export interface TokenomicsProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    CoinDistribution {
+  color?: Colors;
+  showPercent?: (keyof CoinDistribution)[];
+  showLarge?: (keyof CoinDistribution)[];
+}
+
+export interface SocialLinksProps extends React.HTMLAttributes<HTMLDivElement> {
+  align?: "left" | "center" | "right";
+}
