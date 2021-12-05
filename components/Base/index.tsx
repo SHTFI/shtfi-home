5007e92ff0da45791f5cfca92b3b9024bca8d7db
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-05 20:13:42 +0000

    Removed Base components
    Didn't make sense having the base folder as all components are essentialy base and now being assembles in the views foldeR

--- components/Base/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "./Header";
-export { default as Card } from "./Card";
-export { default as Tokenomics } from "./Tokenomics";
-export { default as SocialLinks } from "./SocialLinks";
