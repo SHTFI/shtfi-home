8a2c3efd5cc02b35f545d70b1ca64af4f8a71c11
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-18 12:29:33 +0000

    Add export for MEta component

--- components/ShtfiUi/index.tsx
+++ components/ShtfiUi/index.tsx
@@ -12,3 +12,4 @@ export { default as Header } from "./Header";
 export { default as SocialLinks } from "./SocialLinks";
 export { default as Tokenomics } from "./Tokenomics";
 export { default as Card } from "./Card";
+export { default as Meta } from "./Meta";
