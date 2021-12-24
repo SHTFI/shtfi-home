6b16f3bea4bfaeb907cb07ec1333015750f195be
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-24 14:26:50 +0000

    Implement Web3 unit tests [2 - 5] -- Implement our web3Render method which adds the correct providers for web3 tests [7] -- Import the preloadAll method to allow us to unit test dynamically loaded components [16 - 38] -- Alter the structure of our test data to match the new type defs [42 - 44] -- ADd the preloadAll call [46 - EOF] -- Implement the web3Render method

--- components/Farm/__test__/FarmSelectSection.spec.tsx
+++ components/Farm/__test__/FarmSelectSection.spec.tsx
@@ -1,6 +1,11 @@
-import { render, screen, MatcherFunction } from "utils/testing/testHelpers";
+import {
+  web3Render,
+  screen,
+  waitFor,
+  MatcherFunction,
+} from "utils/testing/testHelpers";
+import preloadAll from "jest-next-dynamic";
 import { FarmSelectSection } from "components";
-import { FarmingPair, FarmToken } from "types";
 
 const TEST_ID = "farmSelect";
 const STAKED_TOKEN: FarmToken = {
@@ -8,7 +13,7 @@ const STAKED_TOKEN: FarmToken = {
   ticker: "STK",
   icon: "/assets/images/tokens/shtfi-125-124.jpeg",
   url: "https://shtfi.io",
-  contract: "0x123123",
+  contract: { 97: "", 56: "" },
   description: "description",
 };
 const REWARD_TOKEN: FarmToken = {
@@ -16,27 +21,43 @@ const REWARD_TOKEN: FarmToken = {
   ticker: "RWD",
   icon: "/assets/images/tokens/squid-75-75.png",
   url: "https://reward.io",
-  contract: "0x123123",
+  contract: { 97: "", 56: "" },
   description: "description",
 };
-const PAIRS: FarmingPair[] = [
-  { stakedToken: STAKED_TOKEN, rewardToken: REWARD_TOKEN },
+const PAIRS: FarmingPairData[] = [
+  {
+    chain: "BSC",
+    contract: { 56: "", 97: "" },
+    stakedToken: {
+      ...STAKED_TOKEN,
+    },
+    rewardToken: {
+      ...REWARD_TOKEN,
+    },
+  },
 ];
+
 describe("<FarmSelectSection />", () => {
-  it("renders", () => {
-    render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
-    const elem = screen.getByTestId(TEST_ID);
-    expect(elem).toBeInTheDocument();
+  // AS we are using dynamically imported components in the web3 instance we need to preload them
+  beforeAll(async () => {
+    await preloadAll();
+  });
+  it("renders", async () => {
+    web3Render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
+    await waitFor(() => {
+      const elem = screen.getByTestId(TEST_ID);
+      expect(elem).toBeInTheDocument();
+    });
   });
   it("renders the logo of each token", () => {
-    render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
+    web3Render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
     const stakedLogo = screen.getByAltText(`${STAKED_TOKEN.name} icon`);
     expect(stakedLogo).toBeInTheDocument();
     const rewardLogo = screen.getByAltText(`${REWARD_TOKEN.name} icon`);
     expect(rewardLogo).toBeInTheDocument();
   });
   it("has meta data saying what to stake and what you get in reward", () => {
-    render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
+    web3Render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
     const getStakedText: MatcherFunction = (content, element) => {
       const stakedText = "Stake:";
       const isStakeParent = content === stakedText && element?.tagName === "P";
@@ -66,7 +87,7 @@ describe("<FarmSelectSection />", () => {
     expect(rewardText).toBeInTheDocument();
   });
   it("says how much of the staked token is in the contract", () => {
-    render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
+    web3Render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
     const getRewardText: MatcherFunction = (content, element) => {
       const contentIsCorrect = content.includes(
         `Total ${STAKED_TOKEN.ticker}:`
@@ -79,7 +100,7 @@ describe("<FarmSelectSection />", () => {
     expect(elem).toBeInTheDocument();
   });
   it("says how much reward token is issued per block", () => {
-    render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
+    web3Render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
     const getBlockRewardText: MatcherFunction = (content, element) => {
       const contentIsCorrect = content.includes(
         `${REWARD_TOKEN.ticker} per block:`
@@ -92,7 +113,7 @@ describe("<FarmSelectSection />", () => {
     expect(elem).toBeInTheDocument();
   });
   it("says reward per staked token", () => {
-    render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
+    web3Render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
     const getBlockRewardText: MatcherFunction = (content, element) => {
       const contentIsCorrect = content.includes(
         `${REWARD_TOKEN.ticker} per ${STAKED_TOKEN.ticker}:`
@@ -105,7 +126,7 @@ describe("<FarmSelectSection />", () => {
     expect(elem).toBeInTheDocument();
   });
   it("shows user their stake", () => {
-    render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
+    web3Render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
     const getUserStakeText: MatcherFunction = (content, element) => {
       const contentIsCorrect = content === `Your Stake:`;
       const sibling = element?.nextElementSibling;
@@ -120,7 +141,7 @@ describe("<FarmSelectSection />", () => {
     expect(elem).toBeInTheDocument();
   });
   it("shows user their rewards", () => {
-    render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
+    web3Render(<FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />);
     const getUserRewardText: MatcherFunction = (content, element) => {
       const contentIsCorrect = content === `Your Rewards:`;
       const sibling = element?.nextElementSibling;
@@ -135,7 +156,7 @@ describe("<FarmSelectSection />", () => {
     expect(elem).toBeInTheDocument();
   });
   it("has a stake, unstake and claim button", () => {
-    const { baseElement } = render(
+    const { baseElement } = web3Render(
       <FarmSelectSection data-testid={TEST_ID} farms={PAIRS} />
     );
     const stakeButton = baseElement.querySelector(
