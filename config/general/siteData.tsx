e476224a58b5e5fe3b4b971577f6608bfa4cb13e
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-26 10:45:21 +0000

    Module to handle all generic site data
    Again, keeping our data stored this way makes it easier for us to handle updates

---
+++ config/general/siteData.tsx
@@ -0,0 +1,10 @@
+export const socialLinksData: SocialLinksData = {
+  github: {
+    profileName: "SHTFI",
+    profileUrl: "https://github.com/SHTFI",
+  },
+  twitter: {
+    profileName: "SHTFI",
+    profileUrl: "https://twitter.com/shtfio",
+  },
+};
