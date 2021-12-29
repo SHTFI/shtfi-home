e3e6dc80d5ecaa28ecfb2d931d896236d77e8f94
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-29 18:51:51 +0000

    Add JSON files for token data

---
+++ config/tokens/core.ts
@@ -0,0 +1,18 @@
+export const core: FarmToken[] = [
+  {
+    ticker: "BTC",
+    icon: "/assets/images/tokens/squid-75-75.png",
+    name: "Bitcoin",
+    url: "https://bitcoin.com",
+    contract: { 56: "", 97: "0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8" },
+    description: "This is a coin that which was rugged.",
+  },
+  {
+    ticker: "SHTFI",
+    icon: "/assets/images/tokens/shtfi-125-125.jpeg",
+    name: "SHTFI Token",
+    url: "https://shtfi.io",
+    contract: { 56: "", 97: "0x72eBF4071F4f367F311ef6aEb659544F401331E3" },
+    description: "This is a coin that which was not rugged.",
+  },
+];
