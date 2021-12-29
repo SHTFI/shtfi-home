0036eff9c04642e83819595e89826ec80238c28e
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-29 18:50:18 +0000

    Add tokens export [4] -- Expot tokens

--- config/index.tsx
+++ config/index.tsx
@@ -1,6 +1,7 @@
 export * from "./general/siteData";
 export * from "./general/navLinks";
 export * from "./farms";
+export * from "./tokens";
 export { default as erc20Abi } from "./abi/erc20.json";
 export { default as shtfiFarmAbi } from "./abi/shitFarm.json";
 export { default as shtfiTokenAbi } from "./abi/shtfiToken.json";
