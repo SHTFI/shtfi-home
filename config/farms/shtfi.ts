ffe1da4b2a1ddb1336ce4cf67d5e03b8925394bc
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-12-29 18:51:27 +0000

    implement tokens as imports [1] - import data [6 - EOF] -- Use array.find to get the information of each token in a way which prevents repeating data input.

--- config/farms/shtfi.ts
+++ config/farms/shtfi.ts
@@ -1,23 +1,19 @@
+import { core, additional } from "config/tokens";
+
 const SHTFI_FARMS: FarmingPairData[] = [
   {
     chain: "BSC",
-    contract: { 56: "", 97: "" },
-    stakedToken: {
-      ticker: "SQUID",
-      icon: "/assets/images/tokens/squid-75-75.png",
-      name: "Squid Token",
-      url: "https://squid.com",
-      contract: { 56: "", 97: "" },
-      description: "This is a coin that which was rugged.",
-    },
-    rewardToken: {
-      ticker: "SHTFI",
-      icon: "/assets/images/tokens/shtfi-125-125.jpeg",
-      name: "SHTFI Token",
-      url: "https://shtfi.io",
-      contract: { 56: "", 97: "" },
-      description: "This is a coin that which was not rugged.",
-    },
+    contract: { 56: "", 97: "0xbAAC754494CC9dE3B1CC43543118ddB6c4b4AaAf" },
+    stakedToken: core.find((token) => token.ticker === "BTC") as FarmToken,
+    rewardToken: core.find((token) => token.ticker === "SHTFI") as FarmToken,
+  },
+  {
+    chain: "BSC",
+    contract: { 56: "", 97: "0xbAAC754494CC9dE3B1CC43543118ddB6c4b4AaAf" },
+    stakedToken: additional.find(
+      (token) => token.ticker === "MOCK"
+    ) as FarmToken,
+    rewardToken: core.find((token) => token.ticker === "SHTFI") as FarmToken,
   },
 ];
 
