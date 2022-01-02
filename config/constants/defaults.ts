ed8806de9c04f8dd52d45b26c865c9d15d7fa581
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-02 09:45:23 +0000

    Add folder for constant variables and a starter file with farm defaults

---
+++ config/constants/defaults.ts
@@ -0,0 +1,23 @@
+export const FARM_INFO: FarmInfo = {
+  stakedAddress: null,
+  address: null,
+  rewardPerStake: 0,
+  stakedBalance: 0,
+  rewardPerBlock: 0,
+  allocPoint: 0,
+  rewardAlloc: 0,
+  startBlock: 0,
+  lastRewardBlock: 0,
+  id: null,
+};
+
+export const DEFAULT_FARMER: FarmerInfo = {
+  userAddress: null,
+  farmId: null,
+  farmAddress: null,
+  stakedBalance: 0,
+  pendingRewards: 0,
+  claimedRewards: 0,
+  totalRewards: 0,
+  lastClaimBlock: 0,
+};
