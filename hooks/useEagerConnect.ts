ec6211b984a73ddf572c2bd9f312d389863b50f5
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-11 07:50:51 +0000

    Update hook to ensure we only run one instance of web3 and that all hooks use the same useWeb3 hook [9] -- GEt the library [15] -- Set a var to prevent infinite loop [16] -- Ensure we exit early if we are already running [23 - 29] -- Check if we are authorised to connect to metamask and do so if we are [31 - 35] -- Check if we are mounted before we try to alter state [36 - 38] -- Cleanup after ourselves to prevent memory leaks

--- hooks/useEagerConnect.ts
+++ hooks/useEagerConnect.ts
@@ -6,36 +6,44 @@ import { injectedWeb3 } from "context";
  */
 const useEagerConnect = (): boolean => {
   // Get the active and activate methods from the web3 lib
-  const { activate, active } = useWeb3();
+  const { activate, active, library } = useWeb3();
   // Prevent an infinite loop by setting a state to determine if we have tried to connect already
   const [tried, setTried] = useState<boolean>(false);
   const [trying, setTrying] = useState<boolean>(false);
   // Use effect to attempt to connect on mount
   useEffect(() => {
+    let mounted = true;
     // Stop infinite loops
-    if (trying || tried || active) return;
+    if (trying || tried || active || library?.provider.isMetaMask) return;
     // Ensure activate is a function
     if (typeof activate !== "function") return;
     // Stop excessive trying
     setTrying(true);
     // Check if the injected connector is already authorised
-    injectedWeb3
-      .isAuthorized()
-      .then(async (auth: boolean) => {
-        // Are we authorised?
-        if (auth === true) {
-          // if so activate our instance
-          await activate(injectedWeb3, undefined, true).catch((e) =>
-            console.warn(e)
-          );
-        }
-      })
-      .finally(() => {
-        // set tried to true and wait until we are activated
-        setTrying(false);
-        setTried(true);
-      });
-  }, [activate, trying, tried, active]);
+    injectedWeb3.isAuthorized().then(async (auth: boolean) => {
+      // Are we authorised?
+      if (auth === true) {
+        // if so activate our instance
+        activate(injectedWeb3, undefined, true).catch((e) => console.warn(e));
+      }
+    });
+
+    if (mounted) {
+      // set tried to true and wait until we are activated
+      setTrying(false);
+      setTried(true);
+    }
+    return () => {
+      mounted = false;
+    };
+  }, [
+    activate,
+    trying,
+    tried,
+    active,
+    library?.provider.isMetaMask,
+    library?.provider,
+  ]);
   // Use another effect to check if the we are active and, if so, set tried to true
   useEffect(() => {
     if (!tried && active && !trying) {
