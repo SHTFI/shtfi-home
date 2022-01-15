30741470f47bafe73854c86d025f193e11557c12
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-15 14:38:44 +0000

    Add helpers [7 - 8] -- Check whether the chain provided by the window's web3 instance is supported by our app [10 - 17] -- Simple function which takes a network ID and attempts to switch the window's eth instance to use it [19 - 25] -- Helper function to activate a metamask instance which takes the activator function as a callback then sends an injected web3 provider

--- utils/data/metaMask.utils.ts
+++ utils/data/metaMask.utils.ts
@@ -1,2 +1,25 @@
-export const isMetaMaskInstalled = () =>
-  window.ethereum && window.ethereum.isMetaMask;
+import { SUPPORTED_CHAIN_IDS } from "config";
+import { injectedWeb3 } from "context";
+
+export const isMetaMaskInstalled = (): boolean =>
+  window.ethereum && window.ethereum.isMetaMask ? true : false;
+
+export const isSupportedChain = (): boolean =>
+  SUPPORTED_CHAIN_IDS.includes(Number(window?.ethereum?.chainId ?? 0));
+
+export const changeNetwork = async (id: number) => {
+  if (!!window?.ethereum?.request) {
+    await window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: `0x${id.toString(16)}` }],
+    });
+  }
+};
+
+export const connectToMetaMask = (activate: any) => {
+  if (window.ethereum && window.ethereum.isMetaMask) {
+    activate(injectedWeb3, undefined, true).catch((e: unknown) =>
+      console.warn(e)
+    );
+  }
+};
