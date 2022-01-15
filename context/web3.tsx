b9b23bf7870eeac3849125d5e30945d9ca6df8c7
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-15 14:36:37 +0000

    Utilise constants [3] -- Import constants [17 - 24] -- Add support for Binance main net in our nodeUrls array [28] -- Allow all chains from our supported chains constant in our injectec connector [32 - 35] -- Implement our supported chain urls in the network connector also

--- context/web3.tsx
+++ context/web3.tsx
@@ -1,9 +1,6 @@
-import {
-  ExternalProvider,
-  JsonRpcFetchFunc,
-  Web3Provider,
-} from "@ethersproject/providers";
+import { Web3Provider } from "@ethersproject/providers";
 import { InjectedConnector } from "@web3-react/injected-connector";
+import { DEFAULT_CHAIN_ID, SUPPORTED_CHAIN_IDS } from "config";
 import { NetworkConnector } from "connectors/networkConnector";
 
 enum ConnectorNames {
@@ -15,24 +12,27 @@ type GetConnectorsByName = {
   [ConnectorName in ConnectorNames]: InjectedConnector | NetworkConnector;
 };
 
-// Set the chain ID
-const CHAIN_IDS = [97];
-// Set the default chain ID
-const DEFAULT_CHAIN_ID = 97;
 // Set an object with our node urls
 const nodeUrls = {
-  97: [
+  [97 as number]: [
     "https://data-seed-prebsc-1-s1.binance.org:8545",
     "https://data-seed-prebsc-2-s2.binance.org:8545",
+  ],
+  [56 as number]: [
+    "https://bsc-dataseed.binance.org/",
+    "https://bsc-dataseed1.defibit.io/",
   ],
 };
 // Make an injected web3 connector to connect to meta mask
 export const injectedWeb3 = new InjectedConnector({
-  supportedChainIds: CHAIN_IDS,
+  supportedChainIds: SUPPORTED_CHAIN_IDS,
 });
 // Make a network connector to allow us to show blockchain data without the user bringing meta mask
 export const networkWeb3 = new NetworkConnector({
-  urls: { [DEFAULT_CHAIN_ID]: nodeUrls[DEFAULT_CHAIN_ID][0] },
+  urls: SUPPORTED_CHAIN_IDS.reduce((op: any, id: number) => {
+    op[id] = nodeUrls[id][0];
+    return op;
+  }, {}),
   defaultChainId: DEFAULT_CHAIN_ID,
 });
 // Export an object which allows us to get the connector by name
